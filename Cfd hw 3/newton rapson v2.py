ddce30813b26924f5efc1d18f6863fe3077c6107
Bryan0066 <38561548+Bryan0066@users.noreply.github.com>
2018-06-06 13:13:13 -0400

    derivative issue solved, cool beans

--- Cfd hw 3/newton rapson v2.py
+++ Cfd hw 3/newton rapson v2.py
@@ -1,39 +1,45 @@
 from prettytable import PrettyTable
-import numpy as math
+import sympy as sym
 from sympy import *
+import math as math
 
 XL, XU, p, mu, D = .00001, 1, 1.23, 1.79*10**-5, .5
 
-List1 = ["", 1, 0.5411, 0.4588, 0.2512, 0.2075, 0.1239, 0.0836, 0.2075, 0.4588]
+List1 = ["", 1, 0.50411, 0.4588, 0.2512, 0.2075, 0.1239, 0.0836, 0.2075, 0.4588]
 
 j = 7
 
-V = (4*List1[j])/(math.pi*D**2)
+V = (4*List1[j])/(3.14*D**2)
 
 Re = (p*V*D)/mu
 
 
-def func(f):
-    return float(4*math.log10(Re*math.sqrt(f))-.4-(1/math.sqrt(f)))
+def func(y):
+    return 1-(400/(9.81*(3*y+(y**2)/2)**3))*(3+y)
 
 
-def dxfunc(f):
-    return float((1/(2*f**(3/2)))+(.868589/f))
+f = sym.symbols('f')
+
+fprime = sym.diff(func(f),f)
+
+
+print('f prime = ',(fprime))
+
 
 
 t = PrettyTable(["Iteration", "X", "EA"])  # headers
 
-EA, x, xp, i = 100, float(.0035), float(.003), 0
+EA, x, xp, i = 100, float(.5), float(2.5), 0
 
-t.add_row((i, "%.6f" % x, "%.6f" % EA))
+t.add_row((i, x, EA))
 
 
 while EA > .001:
     i = i + 1
-    z = float(func(x))
-    g = float(dxfunc(x))
-    x1 = float(x-(z/g))
+    z = func(x)
+    g = fprime.evalf(subs={f: x})
+    x1 = x - (z / g)
     EA = abs((x1 - x) / x1) * 100
     x = x1
-    t.add_row([i, "%.6f" % x, "%.6f" % EA])
+    t.add_row([i, x, EA])
 print(t)
