2663b1e08ba9085fec1e9607119b3cbd6777ab6f
Bryan0066 <38561548+Bryan0066@users.noreply.github.com>
2018-06-08 01:56:12 -0400

    unfinished, everything works but the function?

--- Cfd hw 3/newton rapson v2.py
+++ Cfd hw 3/newton rapson v2.py
@@ -1,43 +1,29 @@
 from prettytable import PrettyTable
+import math
 import sympy as sym
-from sympy import *
-import math as math
-
-XL, XU, p, mu, D = .00001, 1, 1.23, 1.79*10**-5, .5
-
-List1 = ["", 1, 0.50411, 0.4588, 0.2512, 0.2075, 0.1239, 0.0836, 0.2075, 0.4588]
-
-j = 7
-
-V = (4*List1[j])/(3.14*D**2)
-
-Re = (p*V*D)/mu
-
-
-def func(y):
-    return 1-(400/(9.81*(3*y+(y**2)/2)**3))*(3+y)
-
-
-f = sym.symbols('f')
-
-fprime = sym.diff(func(f),f)
-
-
-print('f prime = ',(fprime))
 
 
 
-t = PrettyTable(["Iteration", "X", "EA"])  # headers
-
-EA, x, xp, i = 100, float(.5), float(2.5), 0
-
-t.add_row((i, x, EA))
+Wo, L, E, y, I, pi, EA, x, i = 15, 3, 70, .009, 52.9*10**-6, math.pi, 100, 1.8, 0
 
 
-while EA > .001:
+def func(k):
+    return ((Wo*L)/(3*pi**4*E*I))*(48*L**3*sym.cos((pi/(2*L))*k)-(48*L**3)+(3*pi**3*L*k**2)-(pi**3*k**3))-y
+
+
+v = sym.symbols('v')
+
+fprime = sym.diff(func(v), v)
+#  derives function
+print(fprime)
+t = PrettyTable(["Iteration", "X", "Error"])  # headers for table
+
+t.add_row((i, x, EA))   # adds row to table created by PrettyTable
+
+while EA > .01:
     i = i + 1
     z = func(x)
-    g = fprime.evalf(subs={f: x})
+    g = fprime.evalf(subs={v: x})
     x1 = x - (z / g)
     EA = abs((x1 - x) / x1) * 100
     x = x1
