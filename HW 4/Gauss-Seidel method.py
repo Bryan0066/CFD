57d12f80524540c5b96eaca139e1b906c4b7fe63
Bryan0066 <38561548+Bryan0066@users.noreply.github.com>
2018-06-19 23:36:45 -0400

    new and improved

--- HW 4/Gauss-Seidel method.py
+++ HW 4/Gauss-Seidel method.py
@@ -1,82 +1,80 @@
 
-from numpy import matrix
 import numpy as np
-from numpy.linalg import inv
+from prettytable import PrettyTable
+import pprint as p
 
-import numpy as np
-from scipy.linalg import solve
-'''
-A = np.array([[-2,1,2,0,0,0],[0,0,2,1,2,0],[0,0,0,0,-2,3],[1,1,0,0,0,0],[0,-1,1,1,0,0],[0,0,0,-1,1,1]])
+Q2, Q3, Q4, Q5, Q6, Q7, lam = 0, 0, 0, 0, 0, 0, .95
+x1, x2, x3 = 0,0,0
+x = 0
 
-b =np.array([[0,0,0,1,0,0]])
+a = np.array([[-2,1,2,0,0,0],[1,1,0,0,0,0],[0,1,-1,1,0,0],[0,0,0,1,-1,-1],[0,0,-2,1,2,0],[0,0,0,0,2,3]])
 
+b =np.array([[0],[1],[0],[0],[0],[0]])
+A = np.array([0,0,0,0,0,0])
+p.pprint(A)
+p.pprint(b)
 
+while x <= 2:
 
-
-
-x1 = (1/(A[0,0]))(b[0]-A[2,1])
-
-
-'''
-
-
-'''
-A = np.array([[16,3],[7, -11]])
-b = np.array([[11],[13]])
-
-L = np.array([[16,0],[7,-11]])
-U = np.array([[0,3],[0,0]])
-Lin = inv(L)
+    Q2p = (-Q3-2*Q4)/-2
+    Q2 = lam*Q2p+(1-lam)*Q2
 
 
+    Q3p = (-1+Q2)/-1
+    Q3 = lam*Q3p+(1-lam)*Q3
 
 
+    Q4p = (-Q5-2*Q6)/-2
+    Q4 = lam *Q4p+(1-lam)*Q4
+
+
+    Q5p = (-Q3+Q4)/-1
+    Q5 = lam*Q5p+(1-lam)*Q5
+
+
+    Q6p = (-3*Q7)/-2
+    Q6 = lam*Q6p+(1-lam)*Q6
+
+
+    Q7p = (-Q5+Q6)/-1
+    Q7 = lam*Q7p+(1-lam)*Q7
+
+
+
+    newrow = [Q2, Q3, Q4, Q5, Q6, Q7]
+    A = np.vstack([A,newrow])
+
+    x += 1
 print(A)
-print(b)
-print(Lin)'''
-'''
-b =np.array([[0,0,0,1,0,0]])
-
-Q1 = 1
-Q2 = 0
-Q3 = 0
-Q4 = 0
-Q5 = 0
-Q6 = 0
-Q7 = 0
-
-
-
-Q1 = Q2 + Q3
-Q3 = Q4 + Q5
-Q4 = (Q5+2*Q6)/2
-Q5 = 2*(Q6-Q4)
-Q6 = (2*Q4-Q5)/2
-Q7 = 
-'''
-
-
-
-"""""
-A = np.array([[-2,1,2,0,0,0],[0,0,2,1,2,0],[0,0,0,0,-2,3],[1,1,0,0,0,0],[0,-1,1,1,0,0],[0,0,0,-1,1,1]])
-
-b =np.array([[0],[0],[0],[1],[0],[0]])
-
-x = solve(A,b)
-
-print(np.dot(A,x)-b)
-
-
-x = np.linalg.lstsq(A,b)
-
-print(x)"""
-
-
-
-
-# -*- coding: utf-8 -*-
-
 """
-Gauss Seidelova methoda.
-"""
+    x += 1
+    x1s = (3+x2+x3)/6
+    x1p = lam * (x1s) + (1 - lam) * x1
+
+
+    x2s = (40 - 6*x1p - x3) / 9
+    x2p = lam * (x2s) + (1 - lam) * x2
+
+    x3s = (50 + 3*x1p - x2p) / 12
+    x3p = lam * (x3s) + (1 - lam) * x3
+
+    x1 = x1p
+    x2 = x2p
+    x3 = x3p
+    x +=1
+    print(x1, x2, x3)
+
+0.505882352941176
+0.494117647058824
+0.258823529411765
+0.235294117647059
+0.141176470588235
+0.0941176470588235"""
+'''
+c+2d-2b=0
+e+2f-2d=0
+3g-2f=0
+b+c=1
+c=d+e
+e=f+g'''
 
